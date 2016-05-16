5579f9d5dc6b16d6916f0e0a12093dfd5b419019
O-de-MAX <maria.patrusheva@gmail.com>
2016-05-16 19:50:53 +0500

    Правки, ошибки

---
+++ js/script.js
@@ -0,0 +1,71 @@
+
+
+var congratulationsLink = document.querySelector(".btn-to-basket");
+var congratulationsPopup = document.querySelector(".added-to-card");
+var congratulationsClose = congratulationsPopup.querySelector(".modal-content-close");
+
+congratulationsLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  congratulationsPopup.classList.add("modal-content-show");
+});
+
+congratulationsClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  congratulationsPopup.classList.remove("modal-content-show");
+});
+
+
+var feedbackLink = document.querySelector(".btn-write-us");
+var feedbackPopup = document.querySelector(".modal-content-write-us");
+var feedbackClose = feedbackPopup.querySelector(".modal-content-close");
+var form = feedbackPopup.querySelector(".modal-content-form");
+var name = feedbackPopup.querySelector("[name=name]");
+var mail = feedbackPopup.querySelector("[name=mail]");
+
+feedbackLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedbackPopup.classList.add("modal-content-show");
+  name.focus();
+});
+feedbackClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedbackPopup.classList.remove("modal-content-show");
+});
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  if (!name.value || !mail.value) {
+    event.preventDefault();
+    console.log("Нужно ввести имя и e-mail");
+  }
+});
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (feedbackPopup.classList.contains("modal-content-show")) {
+      feedbackPopup.classList.remove("modal-content-show");
+    }
+  }
+});
+
+
+
+var mapOpen = document.querySelector(".js-open-map");
+var mapPopup = document.querySelector(".modal-content-map");
+var mapClose = mapPopup.querySelector(".modal-content-close");
+
+mapOpen.addEventListener("click", function(event) {
+ event.preventDefault();
+ mapPopup.classList.add("modal-content-show");
+});
+
+mapClose.addEventListener("click", function(event) {
+ event.preventDefault();
+ mapPopup.classList.remove("modal-content-show");
+});
+
+window.addEventListener("keydown", function(event) {
+ if (event.keyCode === 27) {
+   if (mapPopup.classList.contains("modal-content-show")) {
+     mapPopup.classList.remove("modal-content-show");
+   }
+ }
+});
